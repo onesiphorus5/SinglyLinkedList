bb3eac7a135e2aa2d6583d44c59af620e38f126d
Onesiphorus <ndayish.ones@gmail.com>
2024-01-04 15:49:11 +0200

    Adding an iterator class.

--- src/list.h
+++ src/list.h
@@ -1,4 +1,8 @@
 #include <initializer_list>
+#include <iterator>
+
+// For debugging
+#include <iostream>
 
 template <class T>
 class SinglyLinkedList{
@@ -13,6 +17,36 @@ private:
     Node* head_node;
     Node* tail_node;
     std::size_t list_size;
+
+    struct iterator{
+        // Properties
+        using iterator_category = std::forward_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+
+        // Shortand
+        using value_type = Node;
+        using pointer = Node*;
+        using reference = Node&;
+
+        // Constructor
+        iterator( pointer ptr ) : t_ptr{ptr} {}
+
+        // Operators
+        bool operator!=( iterator iter ) {
+            return this->t_ptr != iter.t_ptr;
+        }
+        iterator operator++() {
+            this->t_ptr = this->t_ptr->next;
+            return iterator( this->t_ptr );
+        }
+        T operator*() {
+            return this->t_ptr->t_value;
+        }
+
+    private:
+        pointer t_ptr;
+    };
+
 
 public:
     SinglyLinkedList();
@@ -33,4 +67,8 @@ public:
     void view_list(); // for debugging
 
     // TODO: implement insert()
+
+    // iterator
+    iterator begin() { return iterator( head_node ); }
+    iterator end() { return iterator( nullptr ); }
 };
